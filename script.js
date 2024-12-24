7bb8353d20fa5e0a2abe74059db9a8b84cb3a496
Hafsa-Kanwal <hafsakanwal011@gmail.com>
2024-12-24 05:08:43 -0800

    To-Do-List-Application Using HTML,CSS,JavaScript

--- script.js
+++ script.js
@@ -1,20 +1,87 @@
-// access elements from DOM
-const counterDisplay =  document.getElementById('counter');
-const increaseBtn =  document.getElementById('increment');
-const decreaseBtn =  document.getElementById('decrement');
+const taskInput = document.getElementById('text-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
 
-//now innitialize the counter
-let count=0;
+let tasks = [];
 
-//icrementation  we will add event Listeners
+// Add event listener to Add task btn
+addTaskBtn.addEventListener('click', function(){
+  const taskText = taskInput.value.trim();  ///get the input value and remoove extra spaces
 
-increaseBtn.addEventListener('click', function(){
-    count +=  1;
-    counterDisplay.textContent = count;
+  if(taskText !== ""){
+    tasks.push({text : taskText , completed:false});
+    renderTasks();
+    saveTasks();
+    taskInput.value = ""
+  }
+  else{ 
+    alert("Please!! First Add Your Tasks")
+  }
+    
+
+})
+
+
+//create the renderTasks() function
+function renderTasks(){
+  taskList.innerHTML = "";
+  tasks.forEach((task,index)=>{
+    const li = document.createElement("li")
+    li.classList.toggle("done",task.completed); //add  taskclass if completed
+
+
+    const taskText = document.createElement('span');
+    taskText.textContent = task.text;
+    li.appendChild(taskText)
+
+    //Done Button
+    const doneBtn = document.createElement("button")
+    doneBtn.textContent = "Done";
+    doneBtn.addEventListener('click' , function(){
+      task.completed = !task.completed;
+      renderTasks();
+      saveTasks();
+    });
+
+    //delete btn
+    const deteteBtn = document.createElement('button');
+    deteteBtn.textContent= 'X';
+    deteteBtn.classList.add("delete-Btn");
+    deteteBtn.addEventListener('click', function(){
+      tasks.splice(index,1);
+      renderTasks();
+      saveTasks();
+
+    });
+
+    li.appendChild(doneBtn);
+    li.appendChild(deteteBtn);
+    taskList.appendChild(li);
+
+
+
+  });
+
+  
 }
-)
-decreaseBtn.addEventListener('click', function(){
-    count -=  1;
-    counterDisplay.textContent = count;
+
+
+//save task in to local storage
+function saveTasks(){
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-)
+
+//load task from local storage
+
+function loadTasks(){
+  const savedTask = JSON.parse(localStorage.getItem(tasks));
+  if(saveTasks){
+    tasks = saveTasks;
+    renderTasks();
+
+
+  }
+
+
+}
+window.onload(loadTasks);
